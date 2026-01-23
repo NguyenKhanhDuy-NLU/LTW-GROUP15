cfebf55a088a2bf12f99f1d8453fd3d64926de42
Nguyen Minh Hung <23130128@st.hcmuaf.edu.vn>
2026-01-23 22:10:07 +0700

    Update RoomDetailController.java

--- src/main/java/vn/edu/nlu/fit/demo1/controller/RoomDetailController.java
+++ src/main/java/vn/edu/nlu/fit/demo1/controller/RoomDetailController.java
@@ -1,52 +1,38 @@
 package vn.edu.nlu.fit.demo1.controller;
 
-
 import vn.edu.nlu.fit.demo1.model.Room;
 import vn.edu.nlu.fit.demo1.service.RoomService;
-
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-
 import java.io.IOException;
 
-@WebServlet(name = "RoomDetailController", urlPatterns = {"/room-detail"}
-)
+@WebServlet(name = "RoomDetailController", urlPatterns = {"/room-detail"})
 public class RoomDetailController extends HttpServlet {
-
     private RoomService roomService = new RoomService();
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
-            throws ServletException, IOException {
-
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         String idParam = req.getParameter("id");
-
-        if (idParam == null || idParam.isEmpty()) {
-            resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "Thiếu room id");
+        if (idParam == null) {
+            resp.sendError(400, "Missing Room ID");
             return;
         }
 
-        int id;
         try {
-            id = Integer.parseInt(idParam);
+            int roomId = Integer.parseInt(idParam);
+            Room room = roomService.getRoomDetail(roomId);
+
+            if (room != null) {
+                req.setAttribute("room", room);
+                req.getRequestDispatcher("/WEB-INF/views/room-detail.jsp").forward(req, resp);
+            } else {
+                resp.sendError(404, "Room not found");
+            }
         } catch (NumberFormatException e) {
-            resp.sendError(HttpServletResponse.SC_BAD_REQUEST, "room id không hợp lệ");
-            return;
+            resp.sendError(400, "Invalid Room ID");
         }
-
-        Room room = roomService.getRoomById(id);
-
-        if (room == null) {
-            resp.sendError(HttpServletResponse.SC_NOT_FOUND, "Không tìm thấy Room");
-            return;
-        }
-
-        req.setAttribute("room", room);
-        req.getRequestDispatcher("/WEB-INF/views/room-detail.jsp")
-                .forward(req, resp);
     }
 }
-
