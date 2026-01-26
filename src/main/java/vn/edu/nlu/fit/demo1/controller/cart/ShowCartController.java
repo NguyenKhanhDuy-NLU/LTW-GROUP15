d24dbb68dcfae3bfaf41e087972bde37a953d4a8
Nguyen Minh Hung <23130128@st.hcmuaf.edu.vn>
2026-01-26 17:17:25 +0700

    Create ShowCartController.java

---
+++ src/main/java/vn/edu/nlu/fit/demo1/controller/cart/ShowCartController.java
@@ -0,0 +1,16 @@
+package vn.edu.nlu.fit.demo1.controller.cart;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "ShowCartController", urlPatterns = "/cart")
+public class ShowCartController extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getRequestDispatcher("/WEB-INF/views/cart.jsp").forward(req, resp);
+    }
+}
