6cf0fdcf79c53876327041098f86d66c01b332b1
Nguyen Minh Hung <23130128@st.hcmuaf.edu.vn>
2026-01-26 17:14:00 +0700

    Create Cart.java

---
+++ src/main/java/vn/edu/nlu/fit/demo1/cart/Cart.java
@@ -0,0 +1,55 @@
+package vn.edu.nlu.fit.demo1.cart;
+
+import vn.edu.nlu.fit.demo1.model.Room;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class Cart implements Serializable {
+    private static final long serialVersionUID = 1L;
+
+    private Map<Integer, CartItem> data = new HashMap<>();
+
+    public void add(Room room, int quantity) {
+        if (data.containsKey(room.getId())) {
+            CartItem item = data.get(room.getId());
+            item.setQuantity(item.getQuantity() + quantity);
+        } else {
+            data.put(room.getId(), new CartItem(room, quantity));
+        }
+    }
+
+    public void update(int id, int quantity) {
+        if (data.containsKey(id)) {
+            if (quantity < 1) {
+                data.remove(id);
+            } else {
+                data.get(id).setQuantity(quantity);
+            }
+        }
+    }
+
+    public CartItem deleteRoom(int id) {
+        return data.remove(id);
+    }
+
+    public List<CartItem> removeAll() {
+        List<CartItem> removedItems = new ArrayList<>(data.values());
+        data.clear();
+        return removedItems;
+    }
+
+    public List<CartItem> getList() {
+        return new ArrayList<>(data.values());
+    }
+
+    public double getTotalMoney() {
+        double total = 0;
+        for (CartItem item : data.values()) {
+            total += item.getTotalPrice();
+        }
+        return total;
+    }
+}
