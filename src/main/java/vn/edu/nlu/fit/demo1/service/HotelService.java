d26122896fef0f51a5b2d3b81f58d6266282e179
Duy <23130076@st.hcmuaf.edu.vn>
2026-01-28 06:51:16 +0700

    Update HotelService.java

--- src/main/java/vn/edu/nlu/fit/demo1/service/HotelService.java
+++ src/main/java/vn/edu/nlu/fit/demo1/service/HotelService.java
@@ -1,60 +1,29 @@
 package vn.edu.nlu.fit.demo1.service;
 
-import vn.edu.nlu.fit.demo1.dao.HotelDAO;
-import vn.edu.nlu.fit.demo1.model.Hotel;
+import vn.edu.nlu.fit.demo1.dao.SearchHotelDAO;
+import vn.edu.nlu.fit.demo1.model.SearchHotelCard;
 
-import java.math.BigDecimal;
-import java.util.ArrayList;
 import java.util.List;
 
-public class HotelService {
-    private final HotelDAO hotelDAO = new HotelDAO();
+public class SearchHotelService {
 
-    public List<Hotel> searchByFilter(
+    private final SearchHotelDAO dao = new SearchHotelDAO();
+
+    public List<SearchHotelCard> getAllHotels() {
+        return dao.getAllHotels();
+    }
+
+
+    public List<SearchHotelCard> searchHotels(
             Integer cityId,
-            String checkin,
-            String checkout,
-            Integer guests,
             Integer minPrice,
             Integer maxPrice,
-            List<String> starRatings,
-            List<String> amenityFilters
+            List<Integer> stars,
+            List<String> amenities
     ) {
-        List<Integer> stars = null;
-        if (starRatings != null && !starRatings.isEmpty()) {
-            stars = new ArrayList<>();
-            for (String s : starRatings) {
-                try {
-                    stars.add(Integer.parseInt(s));
-                } catch (NumberFormatException e) {
-                    System.err.println("Invalid star rating: " + s);
-                }
-            }
-        }
-
-        BigDecimal minPriceBD = (minPrice != null) ? BigDecimal.valueOf(minPrice) : null;
-        BigDecimal maxPriceBD = (maxPrice != null) ? BigDecimal.valueOf(maxPrice) : null;
-
-        return hotelDAO.advancedSearch(
-                cityId,
-                checkin,
-                checkout,
-                guests,
-                minPriceBD,
-                maxPriceBD,
-                stars,
-                amenityFilters
-        );
+        return dao.searchHotels(cityId, minPrice, maxPrice, stars, amenities);
     }
-
-    public List<Hotel> searchHotelsByCityId(int cityId, String checkin, String checkout) {
-        return hotelDAO.advancedSearch(cityId, checkin, checkout, null, null, null, null, null);
-    }
-    public List<Hotel> searchHotels(String location, String checkin, String checkout) {
-        String searchLocation = (location != null) ? location.trim() : "";
-        return hotelDAO.searchHotels(searchLocation, checkin, checkout);
-    }
-    public Hotel getHotelById(int id) {
-        return hotelDAO.getHotelById(id);
+    public SearchHotelCard getById(int id) {
+        return dao.getById(id);
     }
 }
