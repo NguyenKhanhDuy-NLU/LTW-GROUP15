49f8a12406c54b1e19e60d9b88a2a3fd145de3d1
Nguyen Minh Hung <23130128@st.hcmuaf.edu.vn>
2026-01-28 06:16:48 +0700

    Update RoomDetailController.java

--- src/main/java/vn/edu/nlu/fit/demo1/controller/RoomDetailController.java
+++ src/main/java/vn/edu/nlu/fit/demo1/controller/RoomDetailController.java
@@ -1,6 +1,8 @@
 package vn.edu.nlu.fit.demo1.controller;
 
+import vn.edu.nlu.fit.demo1.model.HotelDetail; // Import HotelDetail
 import vn.edu.nlu.fit.demo1.model.Room;
+import vn.edu.nlu.fit.demo1.service.HotelDetailService; // Import Service
 import vn.edu.nlu.fit.demo1.service.RoomService;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -12,6 +14,7 @@ import java.io.IOException;
 @WebServlet(name = "RoomDetailController", urlPatterns = {"/room-detail"})
 public class RoomDetailController extends HttpServlet {
     private RoomService roomService = new RoomService();
+    private HotelDetailService hotelDetailService = new HotelDetailService();
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -23,10 +26,19 @@ public class RoomDetailController extends HttpServlet {
 
         try {
             int roomId = Integer.parseInt(idParam);
+
             Room room = roomService.getRoomDetail(roomId);
 
             if (room != null) {
+                HotelDetail hotel = hotelDetailService.getHotelFullDetail(room.getHotelId());
+
                 req.setAttribute("room", room);
+                
+                if (hotel != null) {
+                    req.setAttribute("hotelName", hotel.getHotelName());
+                    req.setAttribute("hotelId", hotel.getId());
+                }
+
                 req.getRequestDispatcher("/WEB-INF/views/room-detail.jsp").forward(req, resp);
             } else {
                 resp.sendError(404, "Room not found");
